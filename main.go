07d7c48f57adeb0d39d52e6551e9ef53318b5235
Jim Reed <jim.reed@qlik.com>
2018-04-07 13:48:23 -0400

    added charts
    added charts

--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import (
 	"encoding/json"
 	"io"
 	"io/ioutil"
+	"math/rand"
 	"net/http"
 	"os"
+	"strconv"
+	"time"
 )
 
 // ItemList : list of items
@@ -16,24 +19,71 @@ type ItemList []struct {
 func main() {
 	http.HandleFunc("/items", items)
 	http.HandleFunc("/test", test)
+	http.HandleFunc("/", health)
 	http.ListenAndServe(":8080", nil)
 }
 
+func health(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "{\"message\":\"OK\"}\n")
+}
+
 func items(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "[{\"item\":\"apple\"}, {\"item\":\"orange\"}, {\"item\":\"pear\"}]\n")
+	responseTime, err := strconv.Atoi(os.Getenv("RESPONSE_TIME"))
+	if err != nil {
+		responseTime = 50
+	}
+
+	failureRate, err := strconv.Atoi(os.Getenv("FAILURE_RATE"))
+	if err != nil {
+		failureRate = 10
+	}
+
+	random := rand.Intn(responseTime * 2)
+	delay := time.Millisecond * time.Duration(random)
+
+	time.Sleep(delay)
+
+	if rand.Intn(100) >= failureRate {
+		io.WriteString(w, "[{\"item\":\"apple\"}, {\"item\":\"orange\"}, {\"item\":\"pear\"}]\n")
+	} else {
+		io.WriteString(w, "[{\"error\":\"true\"}]\n")
+	}
+
 }
 
 func test(w http.ResponseWriter, r *http.Request) {
 
-	if count() == 3 {
-		io.WriteString(w, "{\"success\":\"")
-		io.WriteString(w, "true")
-		io.WriteString(w, "\"}\n")
-	} else {
-		io.WriteString(w, "{\"success\":\"")
-		io.WriteString(w, "false")
-		io.WriteString(w, "\"}\n")
+	iterations, err := strconv.Atoi(os.Getenv("ITERATIONS"))
+	if err != nil {
+		iterations = 100
 	}
+
+	success := 0
+	fail := 0
+
+	start := time.Now()
+
+	for i := 0; i < iterations; i++ {
+		if count() == 3 {
+			success++
+		} else {
+			fail++
+		}
+	}
+
+	t := time.Now()
+	elapsed := t.Sub(start)
+
+	s := strconv.Itoa(success)
+	f := strconv.Itoa(fail)
+
+	io.WriteString(w, "{\"success\":\"")
+	io.WriteString(w, s)
+	io.WriteString(w, "\" , \"fail\":\"")
+	io.WriteString(w, f)
+	io.WriteString(w, "\" , \"elapsed\":\"")
+	io.WriteString(w, elapsed.String())
+	io.WriteString(w, "\"}\n")
 
 }
 
